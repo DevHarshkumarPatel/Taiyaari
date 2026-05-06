1b8bead7279cd150535dfa2fbfab87aa37f46a55
Harsh Patel <harshkumar.patel.dev@gmail.com>
2026-05-06 19:01:22 +0000

    added changes for the abstract method :: HP

---
+++ abstraction_3.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        print("inside Animal ....")
+
+
+class Dog(Animal):
+    # pass
+    def sound(self):
+        print(f"Dog Barks..")
+
+d = Dog()
+
+d.sound()
+
+# now I have inherited the Animal class and also implemented that abstract method inside so after creating object now no error like TypeError, and also we have written something inside that does not matter
